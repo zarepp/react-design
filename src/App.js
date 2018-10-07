07f2dbac080e558dbfa483a5a97374633c48279f
Zharif Othman <zharifothman92@gmail.com>
2018-10-07 16:43:06 +0800

    add source code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.png';
+import badge from './img/badge.png';
+import phone from './img/phone.png';
+import free from './img/free-scan.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="main">
+        <header className="header">
+          <div className="container">
+            <div id="logo">
+              <img src={logo} />
+            </div>
+            <div id="badge">
+              <img src={badge} />
+            </div>
+          </div>
         </header>
+
+        <section id="title">
+          <div className="container">
+            <h1>IS YOUR DEVICE PROTECTED?</h1>
+            <p>FIND OUT WITH OUR FREE SCAN NOW</p>
+          </div>
+        </section>
+
+        <section id="showcase">
+          <div className="container">
+            <img class="img_bottom" src={phone} />
+            <img class="img_top" src={free} />
+          </div>
+        </section>
       </div>
     );
   }
