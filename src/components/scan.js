86707ff770d96fb37c5f6d21138ec4b6b68078a1
Zharif Othman <zharifothman92@gmail.com>
2018-10-09 04:14:19 +0800

    add scan component

---
+++ src/components/scan.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import download from './../img/download.png'
+import clock from './../img/clock-circular-outline.png';
+
+export default class Scan extends Component {
+    render() {
+       return(
+        <section id="action">
+          <div className="container">
+            <div class="box">
+              <div class="instruction">
+                <h1>FREE SCAN ENDS IN</h1>
+              </div>
+              <div class="countdown">
+                <h1>00:15</h1>
+              </div>
+              <div>
+                <button onClick={this.props.updateStatus} class="button_1">SCAN MY DEVICE NOW</button>
+              </div>
+              <div className="list">
+                <ul>
+                  <li>
+                    <img src={download} />
+                    <div>
+                      <h1>78,681 Download</h1>
+                      <p>in the last 7 days</p>
+                    </div>
+                  </li>
+                  <li>
+                    <img src={clock} />
+                    <div>
+                      <h1>Most Recent Download</h1>
+                      <p>Less than a minute ago</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+       );
+    }
+}
