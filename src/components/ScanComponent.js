7a073d5fa752ab35a434e45363685d315d3d649c
Zharif Othman <zharifothman92@gmail.com>
2018-10-11 00:25:45 +0800

    rename component

---
+++ src/components/ScanComponent.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import download from './../img/download.png'
+import clock from './../img/clock-circular-outline.png';
+import free from './../img/free-scan.png';
+
+import DownloadInfo from '../core/DownloadInfo';
+
+
+export default class ScanComponent extends Component {
+    render() {
+       return(
+        <div className="layout">
+          <section id="action">
+
+            <div className="container">
+              <div>
+                <section id="title">
+                  <div className="container">
+                    <p class="header">IS YOUR DEVICE PROTECTED?</p>
+                    <p>FIND OUT WITH OUR FREE SCAN NOW</p>
+                  </div>
+                </section>
+              </div>
+              <div>
+                <img className="image_free" src={free} />
+              </div>
+              <div className="box">
+                <div className="countdown-box">
+                  <div class="message">
+                    <p>FREE SCAN ENDS IN</p>
+                  </div>
+                  <div class="count">
+                    <p>00:15</p>
+                  </div>
+                </div>
+                <div className="button">
+                  <button onClick={this.props.updateStatus} className="button_1">SCAN MY DEVICE NOW</button>
+                </div>
+                <DownloadInfo />
+              </div>
+            </div>
+
+          </section>
+        </div>
+       );
+    }
+}
